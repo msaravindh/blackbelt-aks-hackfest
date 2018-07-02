4589039ac5d4a24ad78fb3f5d70c54dee4a964bd
msaravindh <arsrini@microsoft.com>
2018-07-02 15:39:38 +0530

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Aravindh first
+        Aravindh first second
       </div>
       <div class="col-lg-6">
       </div>
