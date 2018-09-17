c564cc09ced5a0d01f7fb2b02c8256482255a5fb
msaravindh <arsrini@microsoft.com>
2018-09-17 11:11:10 +0530

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Aravindh Third Try
+        Aravindh Fourth Try
       </div>
       <div class="col-lg-6">
       </div>
